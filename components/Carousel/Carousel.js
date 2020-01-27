a6c78a4f5704f9731791cab2742c5798176dcdd0
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-01-27 12:10:24 +0800

    added createCarousel component function

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,43 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const createCarousel = () => {
+  const carousel = document.createElement("div");
+  const leftButton = document.createElement("div");
+  const mountainsImg = document.createElement("img");
+  const computerImg = document.createElement("img");
+  const treesImg = document.createElement("img");
+  const turntableImg = document.createElement("img");
+  const rightButton = document.createElement("div");
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(mountainsImg);
+  carousel.appendChild(computerImg);
+  carousel.appendChild(treesImg);
+  carousel.appendChild(turntableImg);
+  carousel.appendChild(rightButton);
+
+  // set class names
+  carousel.classList.add("carousel");
+  leftButton.classList.add("left-button");
+  rightButton.classList.add("right-button");
+
+  // add content
+  leftButton.textContent = "<";
+  rightButton.textContent = ">";
+
+  mountainsImg.src = "./assets/carousel/mountains.jpeg";
+  computerImg.src = "./assets/carousel/computer.jpeg";
+  treesImg.src = "./assets/carousel/trees.jpeg";
+  turntableImg.src = "./assets/carousel/turntable.jpeg";
+
+  // add functionality
+  leftButton.addEventListener("click", () => {
+    
+  })
+
+  return carousel;
+}
+
+const carouselContainer = document.querySelector(".carousel-container");
